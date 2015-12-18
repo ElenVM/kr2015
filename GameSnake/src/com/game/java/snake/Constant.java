19e9f4033bbec53718757c34a572a7bc8cbd4c0a
ElenVM <elena.v.m.4@mail.ru>
2015-12-18 17:04:57 +0200

    0.0.3
    there was a glitch in the computer

---
+++ GameSnake/src/com/game/java/snake/Constant.java
@@ -0,0 +1,11 @@
+//размеры сцены
+package com.game.java.snake;
+
+/**
+ * Created by Елена Мирошниченко on 18.12.2015.
+ */
+public interface Constant {
+    int WORLD_WIDTH = 12;
+    int WORLD_HEIGHT = 12;
+    int CELL_SIZE = 32;
+}
--- GameSnake/src/com/game/java/snake/DesktopLauncher.java
+++ GameSnake/src/com/game/java/snake/DesktopLauncher.java
@@ -3,11 +3,19 @@ package com.game.java.snake;
 
 import java.awt.*;
 
+import static com.game.java.snake.Constant.*;
+
 /**
  * Created by Елена Мирошниченко on 15.12.2015.
  */
 public class DesktopLauncher {
     public static void main(String[] args) {
+
+        int screenWidth = WORLD_WIDTH * CELL_SIZE;
+        int screenHeight = WORLD_HEIGHT * CELL_SIZE;
+
+        Dimension screenSize = new Dimension(screenWidth, screenHeight);
+
         Game game = DesktopGameBuilder.build(new Dimension(640, 480));
         game.setScene(new MainScene(game));
         game.start();
